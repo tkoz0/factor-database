7373c68fd80b55dd9e9ea9676819cf45eadf4458
tkoz0 <13438573+tkoz0@users.noreply.github.com>
2025-01-30 13:53:02 -0500

    added some options to database insertion script

--- prefactor/dbinsert.py
+++ prefactor/dbinsert.py
@@ -4,6 +4,7 @@
 use data from the dbfactor.py script to insert into database
 '''
 
+import argparse
 import json
 import os
 import sys
@@ -13,9 +14,18 @@ import gmpy2
 scriptdir = os.path.dirname(__file__)
 sys.path.append(f'{scriptdir}/..')
 
-import app.database as db
+parser = argparse.ArgumentParser()
+parser.add_argument('-p','--path',help='provide or override table path',type=str)
+parser.add_argument('-d','--dry-run',help='show actions that would be performed',action='store_true')
+parser.add_argument('-i','--input',help='show input json lines',action='store_true')
+args = parser.parse_args()
+
+if not args.dry_run:
+    import app.database as db
 
 for i,line in enumerate(sys.stdin):
+    if args.input:
+        print(line.strip())
     data = json.loads(line)
     index = data['index']
     assert isinstance(index,int)
@@ -25,17 +35,29 @@ for i,line in enumerate(sys.stdin):
     assert isinstance(expr,str)
     path = data['path']
     assert path is None or isinstance(path,str)
+    if args.path:
+        path = args.path
     factors = data['factors']
     assert factors is None or isinstance(factors,list)
     if factors is not None:
         small_factors: list[int] = [f for f,_,_ in factors if f < 2**64]
         assert all(gmpy2.is_prime(f) for f in small_factors) # type:ignore
-        added,nrow = db.addNumber(value,small_factors)
-        if added:
-            print(f'\033[93mline index {i} added to database, number id {nrow.id}\033[0m')
+        assert all(value % f == 0 for f in small_factors)
+        if not args.dry_run:
+            added,nrow = db.addNumber(value,small_factors)
+            if added:
+                print(f'\033[92mindex {i} added to database, number id {nrow.id}\033[0m')
+            else:
+                print(f'\033[93mindex {i} already in database, number id {nrow.id}\033[0m')
+        else:
+            print(f'insert number {value} with factors {small_factors}')
     if path is not None:
         path = tuple(p for p in path.split('/') if p)
-        try:
-            db.createCategoryNumber(path,index,value,expr)
-        except Exception as e:
-            print(f'\033[31mfailed to add to category: {e}\033[0m')
+        if not args.dry_run:
+            try:
+                db.createCategoryNumber(path,index,value,expr)
+                print(f'\033[32msuccessfully added index {i} to category\033[0m')
+            except Exception as e:
+                print(f'\033[31mfailed to add to category: {e}\033[0m')
+        else:
+            print(f'create /{'/'.join(path)} index {i}')
