9cfac1f9f01939cdb859d1322c70e3fcb2003d42
tkoz0 <13438573+tkoz0@users.noreply.github.com>
2025-07-08 03:22:24 -0400

    group together parts needing to be edited

--- prefactor/dbfactor.py
+++ prefactor/dbfactor.py
@@ -26,58 +26,79 @@ ecm_b1_curves = ((2000,2000),(10000,1000),(50000,500))
 ecm_threads = 0
 
 # ==============================================================================
-# modify the following as necessary
-# (probably just the cmdline args and the 3 lines below a TODO)
+# modify the following section as necessary
 # keep the same output format (jsonl)
 # below is an example for repunits
+# set dry_run = False to actually run the factoring
 # ==============================================================================
+
+dry_run = True
 
 index_beg = int(sys.argv[1])
 index_end = int(sys.argv[2])
+assert 0 <= index_beg <= index_end
 
 base = int(sys.argv[3])
 assert 2 <= base <= 36
 
 def expr(base:int,n:int,/) -> str:
-    # repunit expression
+    # expression to display on factor tables
     assert 2 <= base <= 36 and n >= 0
     return f'\\[2^{{{n}}}-1\\]' if base == 2 \
         else f'\\[{{{base}^{{{n}}}-1\\over{base-1}}}\\]'
 
 def path(base:int,/) -> str:
-    # repunit table path
+    # path to the number table
     assert 2 <= base <= 36
     return f'repunit/{base}'
 
+def value(base:int,n:int,/) -> int:
+    # integer value
+    assert 2 <= base <= 36 and n >= 0
+    return sequences.repunit(base,n)
+
+# ==============================================================================
+
 for n in range(index_beg,index_end):
-    sys.stderr.write(f'\n\033[94mFACTORING INDEX {n}\033[0m\n')
-    # TODO adjust computation of number value
-    v = sequences.repunit(base,n)
+    if not dry_run:
+        sys.stderr.write(f'\n\033[94mFACTORING INDEX {n}\033[0m\n')
+
+    v = value(base,n)
+
+    # number details
     output = {
         'index': n,
         'value': v,
         # TODO adjust expression to display on factor tables
         'expr': expr(base,n),
         # TODO adjust table path
-        'path': path(base),
-        'factors': None if v < 2
+        'path': path(base)
+    }
+
+    # (partial) factorization
+    if not dry_run:
+        output['factors'] = None if v < 2 \
             else prefactor_runner(v,
                                   lim_tdiv=lim_tdiv,
                                   lim_prho=lim_prho,
                                   ecm_b1_curves=ecm_b1_curves,
                                   ecm_threads=ecm_threads,
                                   progress_stream=sys.stderr)
-    }
-    sys.stderr.write(f'\033[32mCOMPLETED INDEX {n}\033[0m\n')
+        sys.stderr.write(f'\033[32mCOMPLETED INDEX {n}\033[0m\n')
 
-    # check result and output
-    if v >= 2:
-        cof = v
-        for f,p,_ in output['factors']:
-            assert gmpy2.is_prime(f) == p # type:ignore
-            assert 1 < f <= cof
-            assert cof % f == 0
-            cof //= f
-        assert cof == 1
-    print(json.dumps(output,separators=(',',':')))
+        # check result
+        if v >= 2:
+            cof = v
+            for f,p,_ in output['factors']: # type:ignore
+                assert gmpy2.is_prime(f) == p # type:ignore
+                assert 1 < f <= cof
+                assert cof % f == 0
+                cof //= f
+            assert cof == 1
+
+    # output
+    if dry_run:
+        print(json.dumps(output,indent=4))
+    else:
+        print(json.dumps(output,separators=(',',':')))
     sys.stdout.flush()
