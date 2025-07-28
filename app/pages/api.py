899912fda31dcc087db0271819bad3ae1c4312b4
tkoz0 <13438573+tkoz0@users.noreply.github.com>
2025-07-28 04:26:09 -0400

    preparation for implementing later

--- app/pages/api.py
+++ app/pages/api.py
@@ -1,12 +1,24 @@
 import json
-
 import quart
 
 import app.util
 
 bp = quart.Blueprint('api',__name__)
 
-@bp.route('/api')
-async def api():
+def toJson(obj,/) -> str:
+    return json.dumps(obj,separators=(',',':'))
+
+#def fromJson(s:str,/):
+#    return json.loads(s)
+
+def jsonResponse(obj,code=200,/) -> quart.Response:
+    return quart.Response(toJson(obj),code,mimetype='application/json')
+
+@bp.get('/api')
+async def apiGet():
     return await quart.render_template('api.jinja',page='api',
                                        **app.util.getPageInfo())
+
+@bp.post('/api')
+async def apiPost():
+    return jsonResponse({'message':'not implemented'},501)
