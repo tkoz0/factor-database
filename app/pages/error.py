dc2802d3fed8fc3d8118d74bdd19c403b67d09c0
tkoz0 <13438573+tkoz0@users.noreply.github.com>
2025-07-28 04:26:53 -0400

    organized comment with error codes

--- app/pages/error.py
+++ app/pages/error.py
@@ -3,16 +3,23 @@ import quart
 bp = quart.Blueprint('error',__name__)
 
 @bp.route('/<path:path>')
-async def error404(path):
+async def otherPage(path):
     # any page not matching a programmed route should give 404
     return await quart.render_template('404.jinja',path=path),404
 
-# codes
+# info codes
+# (usually not used)
+
+# success codes
 # 200 = ok
 # 201 = created
 # 202 = accepted
 # 204 = no content
+
+# redirect codes
 # 301 = moved
+
+# client error
 # 400 = bad request
 # 401 = unauthorized (identity unknown)
 # 403 = forbidden (identity known)
@@ -22,3 +29,10 @@ async def error404(path):
 # 414 = uri too long
 # 418 = im a teapot (april fools joke)
 # 429 = too many requests
+
+# server error
+# 500 = internal
+# 501 = not implemented
+# 502 = bad gateway
+# 503 = unavailable
+# 504 = gateway timeout
