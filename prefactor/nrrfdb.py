944e695a0a847b60a261990f47382640a69f15a2
tkoz0 <13438573+tkoz0@users.noreply.github.com>
2025-08-31 02:38:55 -0400

    generate code to setup categories and tables for nrr

--- prefactor/nrrfdb.py
+++ prefactor/nrrfdb.py
@@ -12,7 +12,7 @@ from nrrpoly import NrrPoly
 
 DIGITS = '0123456789abcdefghijklmnopqrstuvwxyz'
 
-def toInt(base:int,value:str) -> int:
+def toInt(base:int,value:str,/) -> int:
     assert 2 <= base <= 36
     ret = 0
     dval = 1
@@ -23,7 +23,7 @@ def toInt(base:int,value:str) -> int:
         dval *= base
     return ret
 
-def toStr(base:int,value:int) -> str:
+def toStr(base:int,value:int,/) -> str:
     assert 2 <= base <= 36
     ret = ''
     while value:
@@ -32,9 +32,10 @@ def toStr(base:int,value:int) -> str:
     return ret[::-1]
 
 class NrrSetInfo:
-    def __init__(self,regex:str,stdkmd:str,mult:Fraction,poly:NrrPoly,
+    def __init__(self,base:int,regex:str,stdkmd:str,mult:Fraction,poly:NrrPoly,
                  factors:list[NrrPoly],equiv:list[tuple[Fraction,int,str]],
                  expr:str,latex:str):
+        self.base = base
         self.regex = regex
         self.stdkmd = stdkmd
         self.poly_mult = mult
@@ -47,10 +48,10 @@ class NrrSetInfo:
     def addEquiv(self,regex:str,stdkmd:str,mult:Fraction,poly:NrrPoly,
                  factors:list[NrrPoly],equiv:list[tuple[Fraction,int,str]],
                  expr:str,latex:str):
-        self.equiv_nrrs.append(NrrSetInfo(regex,stdkmd,mult,poly,factors,equiv,
-                                          expr,latex))
+        self.equiv_nrrs.append(NrrSetInfo(self.base,regex,stdkmd,mult,poly,
+                                          factors,equiv,expr,latex))
 
-def readNrrs(base:int) -> list[NrrSetInfo]:
+def readNrrs(base:int,/) -> list[NrrSetInfo]:
     assert 2 <= base <= 36
     letters = ' abcdefghijklmnopqrstuvwxyz'
     digit_re = f'[0-{base-1}]' if base <= 10 else f'[0-9a-{letters[base-10]}]'
@@ -101,10 +102,191 @@ def readNrrs(base:int) -> list[NrrSetInfo]:
         assert isinstance(poly_factor_equiv,list)
         equiv = [(Fraction(*k),s,r) for k,s,r in poly_factor_equiv]
         if is_main:
-            ret.append(NrrSetInfo(regex,stdkmd,mult,poly,factors,equiv,
+            ret.append(NrrSetInfo(base,regex,stdkmd,mult,poly,factors,equiv,
                                   expr,latex))
         else:
             ret[-1].addEquiv(regex,stdkmd,mult,poly,factors,equiv,expr,latex)
+    return ret
+
+class NrrPageInfo:
+    def __init__(self,title:str,info:str,repstr:str):
+        self.title = title
+        self.info = info
+        self.repstr = repstr
+
+def _rep(d:str,/) -> str:
+    return f'{d}{d}..{d}{d}'
+
+# where my choices differ from stdkmd
+# map my pattern to stdkmd pattern
+# this is for generating urls for the stdkmd site
+STDKMD_DIFF = {
+    '18884': '94442',
+    '42225': '84445',
+    '14448': '72224',
+    '32225': '64445',
+    '12228': '61114',
+    '12224': '61112',
+    '16664': '49992',
+    '11112': '10008'
+}
+# special pages handled differently
+STDKMD_SPECIAL = {
+    '10001': 'https://stdkmd.net/nrr/repunit/10001.htm',
+    '11111': 'https://stdkmd.net/nrr/repunit/'
+}
+
+def _few(m:Fraction,nrr:NrrPoly) -> list[int]:
+    n,d = m.as_integer_ratio()
+    ret: list[int] = []
+    for i in range(5):
+        v = nrr(i) * n
+        v,rem = divmod(v,d)
+        assert rem == 0
+        ret.append(v)
+    return ret
+
+def titleAndRepstr(s:str) -> tuple[str,str]:
+    d0,d1,d2,d3,d4 = s
+    v0 = DIGITS.find(d0)
+    v1 = DIGITS.find(d1)
+    v2 = DIGITS.find(d2)
+    v3 = DIGITS.find(d3)
+    v4 = DIGITS.find(d4)
+
+    if d0 == d1 == d2 == d3 == d4: # AAAAA
+        A = d0
+        repstr = f'{_rep(A)}'
+        title = f'Repunit {repstr}' if v0 == 1 else f'Repdigit {repstr}'
+    elif d0 == d1 == d2 == d3 and d3 != d4: # AAAAB
+        A,B = d0,d4
+        repstr = f'{_rep(A)}{B}'
+        title = f'Near Repdigit {repstr}'
+    elif d0 != d1 and d1 == d2 == d3 == d4: # ABBBB
+        A,B = d0,d1
+        repstr = f'{A}{_rep(B)}'
+        title = f'Near Repdigit {repstr}'
+    elif d0 == d1 == d2 == d4 and d3 != d4: # AAABA
+        A,B = d0,d3
+        repstr = f'{_rep(A)}{B}{A}'
+        title = f'Near Repdigit {repstr}'
+    elif d0 == d2 == d3 == d4 and d0 != d1: # ABAAA
+        A,B = d0,d1
+        repstr = f'{A}{B}{_rep(A)}'
+        title = f'Near Repdigit {repstr}'
+    elif d0 == d4 and d1 == d2 == d3 and d0 != d1: # ABBBA
+        A,B = d0,d1
+        repstr = f'{A}{_rep(B)}{A}'
+        title = f'Plateau {repstr}' if d0 < d1 \
+            else f'Depression {repstr}'
+    elif d0 != d1 and d1 == d2 == d3 and d3 != d4 and d0 != d4: # ABBBC
+        A,B,C = d0,d2,d4
+        repstr = f'{A}{_rep(B)}{C}'
+        title = f'Quasi Repdigit {repstr}'
+    elif d0 == d1 == d3 == d4 and d1 != d2: # AABAA
+        A,B = d0,d2
+        repstr = f'{_rep(A)}{B}{_rep(A)}'
+        title = f'Near Repdigit Palindrome {repstr}'
+    else:
+        assert 0, nrrset.stdkmd
+    title = f'{nrrset.stdkmd}: {title}'
+
+    return title,repstr
+
+def makePageInfo(nrrset:NrrSetInfo,/) -> NrrPageInfo:
+    base = nrrset.base
+    assert re.fullmatch(f'[{DIGITS[:nrrset.base]}]{{5}}',nrrset.stdkmd)
+    d0,d1,d2,d3,d4 = nrrset.stdkmd
+    v0 = DIGITS.find(d0)
+    v1 = DIGITS.find(d1)
+    v2 = DIGITS.find(d2)
+    v3 = DIGITS.find(d3)
+    v4 = DIGITS.find(d4)
+    assert 0 < v0 < base
+    assert 0 <= v1 < base
+    assert 0 <= v2 < base
+    assert 0 <= v3 < base
+    assert 0 <= v4 < base
+    info = ''
+    title,repstr = titleAndRepstr(nrrset.stdkmd)
+
+    first_terms = _few(nrrset.poly_mult,nrrset.poly)
+    numlist = ','.join(map(str,first_terms)) + ',\\ldots'
+    regex_latex = f'[{nrrset.regex.replace('*','^*')}]'
+    info += f'<p>First few terms (base 10): \\[{regex_latex}_n=\\{{{numlist}\\}}\\]</p>\n'
+    if base != 10:
+        numlist = ','.join(f'\\text{{{toStr(base,v)}}}' for v in first_terms) + ',\\ldots'
+        info += f'<p>First few terms (base {base}): \\[\\{{{numlist}\\}}\\]</p>\n'
+
+    info += f'<p>General Formula:\\[{regex_latex}_n={nrrset.latex}\\]</p>\n'
+
+    if len(nrrset.equiv_nrrs) == 0:
+        info += f'<p>Equivalent Patterns: (none)</p>\n'
+    else:
+        info += f'<p>Equivalent Patterns:'
+        for nrr in nrrset.equiv_nrrs:
+            # nrrset main is nrrset.poly_mult * nrrset.poly
+            # nrr equiv sequence is nrr.poly_mult * nrr.poly
+            assert len(nrr.poly_factors_equiv) == 1
+            k,s,r = nrr.poly_factors_equiv[0]
+            k *= nrr.poly_mult
+            n,d = k.as_integer_ratio()
+            # k * [r](n+s)
+            info += f'\n\\[[{nrr.regex.replace('*','^*')}]_n=' \
+                + (f'{n}\\times' if n != 1 else '') \
+                + f'[{r.replace('*','^*')}]_{f'{{n{s:+}}}' if s != 0 else 'n'}' \
+                + (f'/{d}' if d != 1 else '') \
+                + f'\\]'
+            for i in range(max(0,-s),max(0,-s)+5):
+                # v1 = evaluate other sequence
+                v1 = nrr.poly(i) * nrr.poly_mult.numerator
+                v1,rem = divmod(v1,nrr.poly_mult.denominator)
+                assert rem == 0
+                # v2 = evaluate main sequence with multiply and shift
+                v2 = nrrset.poly_mult.numerator * nrrset.poly(i+s) * n
+                v2,rem = divmod(v2,d*nrrset.poly_mult.denominator)
+                assert rem == 0
+                assert v1 == v2, f'{v1} != {v2}'
+        info += '</p>\n'
+
+    if base == 10: # link to relevant stdkmd page
+        s = nrrset.stdkmd
+        text = f'Factorization of {repstr} on stdkmd.net'
+        if s in STDKMD_SPECIAL:
+            url = STDKMD_SPECIAL[s]
+        else:
+            if s in STDKMD_DIFF:
+                s = STDKMD_DIFF[s]
+                orepstr = titleAndRepstr(s)[1]
+                text = f'Factorization of {orepstr} on stdkmd.net'
+            url = f'https://stdkmd.net/nrr/{s[0]}/{s}.htm'
+        info += f'<p>See Also: <a href="{url}" target="_blank">{text}</a></p>\n'
+
+    return NrrPageInfo(title,info,repstr)
+
+def _nrrfactorstr(nrrset:NrrSetInfo) -> str:
+    assert len(nrrset.poly_factors) > 1
+    ret = f'\\[[{nrrset.regex.replace('*','^*')}]_n={nrrset.latex}='
+    if nrrset.poly_mult.numerator != 1:
+        ret += f'{nrrset.poly_mult.numerator}\\times'
+    for factor in nrrset.poly_factors:
+        ret += f'\\left({factor.latex()}\\right)'
+    if nrrset.poly_mult.denominator != 1:
+        ret += f'/{nrrset.poly_mult.denominator}'
+    ret += f'='
+    if nrrset.poly_mult.numerator != 1:
+        ret += f'{nrrset.poly_mult.numerator}\\times'
+    for k,s,r in nrrset.poly_factors_equiv:
+        ret += f'\\left('
+        if k.numerator != 1:
+            ret += f'{k.numerator}\\times'
+        ret += f'[{r.replace('*','^*')}]_{f'{{n{s:+}}}' if s != 0 else 'n'}'
+        if k.denominator != 1:
+            ret += f'/{k.denominator}'
+        ret += f'\\right)'
+    if nrrset.poly_mult.denominator != 1:
+        ret += f'/{nrrset.poly_mult.denominator}'
+    ret += f'\\]'
     return ret
 
 if __name__ == '__main__':
@@ -119,8 +301,10 @@ if __name__ == '__main__':
                         help='python dict with lambda expressions')
     parser.add_argument('--python-lambdas-and-latex',action='store_true',
                         help='python dict with lambdas and latex string')
-    parser.add_argument('--descriptions',action='store_true',
-                        help='output ')
+    parser.add_argument('--python-make-categories',action='store_true',
+                        help='output code to make the database categories')
+    parser.add_argument('--python-set-descriptions',action='store_true',
+                        help='output code to set the category descriptions')
     args = parser.parse_args()
     assert args.base is not None and 2 <= args.base <= 36
 
@@ -131,15 +315,18 @@ if __name__ == '__main__':
         flag_count += 1
     if args.python_lambdas_and_latex:
         flag_count += 1
-    if args.descriptions:
+    if args.python_make_categories:
+        flag_count += 1
+    if args.python_set_descriptions:
         flag_count += 1
 
     if flag_count != 1:
         sys.stderr.write('expected exactly one of:\n'
-                         '    --pattern-list\n'
-                         '    --python-lambdas\n'
-                         '    --python-lambdas-and-latex\n'
-                         '    --descriptions\n')
+                         '  --pattern-list\n'
+                         '  --python-lambdas\n'
+                         '  --python-lambdas-and-latex\n'
+                         '  --python-make-categories\n'
+                         '  --python-set-descriptions\n')
         exit(1)
 
     nrrdata = sorted(readNrrs(args.base), key = lambda nrrset: nrrset.stdkmd)
@@ -180,5 +367,65 @@ if __name__ == '__main__':
                 #print(f'  # factors to: {nrrset.poly_mult} * {factors_str}')
         print('}')
 
-    if args.descriptions:
-        pass
+    if args.python_make_categories:
+        print('import app.database.categories as dbcat')
+        print()
+        for i in range(1,args.base):
+            d = DIGITS[i]
+            print(f"dbcat.createCategory('stdkmd-nrr/{args.base}/{d}', is_table = False, "
+                  f"title = 'First Digit {d}', info = '')")
+        prev_digit = '0'
+        for nrrset in nrrdata:
+            if len(nrrset.poly_factors) > 1:
+                continue
+            s = nrrset.stdkmd
+            if s[0] != prev_digit:
+                print()
+                prev_digit = s[0]
+            t = nrrset.regex
+            for d in DIGITS[:nrrset.base]:
+                t = t.replace(f'{d}*',_rep(d))
+            print(f"dbcat.createCategory('stdkmd-nrr/{args.base}/{s[0]}/{s}', "
+                  f"is_table = True, title = 'Pattern {s}: {t}', info = '')")
+            #for nrr in nrrset.equiv_nrrs:
+            #    s = nrr.stdkmd
+            #    print(f"dbcat.createCategory('stdkmd-nrr/{s[0]}/{s}', "
+            #          f"is_table = True, title = 'duplicate', info = '')")
+
+    if args.python_set_descriptions:
+        print('import app.database.categories as dbcat')
+        prev_digit = '0'
+        per_digit_factors = {DIGITS[i]:[] for i in range(1,args.base)}
+        for nrrset in nrrdata:
+            s = nrrset.stdkmd
+            if s[0] != prev_digit:
+                print()
+                prev_digit = s[0]
+            if len(nrrset.poly_factors) > 1: # factorable
+                per_digit_factors[s[0]].append(_nrrfactorstr(nrrset))
+                for nrr in nrrset.equiv_nrrs:
+                    per_digit_factors[s[0]].append(_nrrfactorstr(nrr))
+            else: # not factorable
+                detail = makePageInfo(nrrset)
+                print(f"dbcat.setCategoryTitle('stdkmd-nrr/{args.base}/{s[0]}/{s}',"
+                    f"{repr(detail.title)})")
+                lines = detail.info.splitlines()
+                print(f"dbcat.setCategoryInfo('stdkmd-nrr/{args.base}/{s[0]}/{s}',")
+                for i in range(len(lines)):
+                    print(f"    {repr(lines[i]+'\n')}"
+                        + (')' if i == len(lines)-1 else ''))
+        print()
+        for i in range(1,args.base):
+            d = DIGITS[i]
+            print(f"dbcat.setCategoryTitle('stdkmd-nrr/{args.base}/{d}',"
+                  f"'Patterns Starting With {d}')")
+            if len(per_digit_factors[d]) == 0:
+                print(f"dbcat.setCategoryInfo('stdkmd-nrr/{args.base}/{d}',\n"
+                      f"    '<p>Patterns that can be factored: (none)</p>\\n')")
+            else:
+                print(f"dbcat.setCategoryInfo('stdkmd-nrr/{args.base}/{d}',\n"
+                      f"    '<p>Patterns that can be factored:\\n'")
+                last_i = len(per_digit_factors[d])-1
+                for i,data in enumerate(per_digit_factors[d]):
+                    print(f"    {repr(data + ('</p>' if i == last_i else '') + '\n')}"
+                          + (')' if i == last_i else ''))
