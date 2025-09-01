b593c009801de2531c19cb720b2f723dc10fd9d3
tkoz0 <13438573+tkoz0@users.noreply.github.com>
2025-09-01 02:02:28 -0400

    format changes

--- prefactor/nrrfdb.py
+++ prefactor/nrrfdb.py
@@ -193,6 +193,11 @@ def titleAndRepstr(s:str) -> tuple[str,str]:
 
     return title,repstr
 
+def _seqsym(base:int,regex:str) -> str:
+    regex = regex.replace('*','^*')
+    regex = ''.join(f'\\text{{{d}}}' if d in DIGITS[10:] else d for d in regex)
+    return f'[{regex}_{f'{base}' if base < 10 else f'{{{base}}}'}]'
+
 def makePageInfo(nrrset:NrrSetInfo,/) -> NrrPageInfo:
     base = nrrset.base
     assert re.fullmatch(f'[{DIGITS[:nrrset.base]}]{{5}}',nrrset.stdkmd)
@@ -212,7 +217,7 @@ def makePageInfo(nrrset:NrrSetInfo,/) -> NrrPageInfo:
 
     first_terms = _few(nrrset.poly_mult,nrrset.poly)
     numlist = ','.join(map(str,first_terms)) + ',\\ldots'
-    regex_latex = f'[{nrrset.regex.replace('*','^*')}]'
+    regex_latex = _seqsym(base,nrrset.regex)
     info += f'<p>First few terms (base 10): \\[{regex_latex}_n=\\{{{numlist}\\}}\\]</p>\n'
     if base != 10:
         numlist = ','.join(f'\\text{{{toStr(base,v)}}}' for v in first_terms) + ',\\ldots'
@@ -232,11 +237,12 @@ def makePageInfo(nrrset:NrrSetInfo,/) -> NrrPageInfo:
             k *= nrr.poly_mult
             n,d = k.as_integer_ratio()
             # k * [r](n+s)
-            info += f'\n\\[[{nrr.regex.replace('*','^*')}]_n=' \
-                + (f'{n}\\times' if n != 1 else '') \
-                + f'[{r.replace('*','^*')}]_{f'{{n{s:+}}}' if s != 0 else 'n'}' \
-                + (f'/{d}' if d != 1 else '') \
-                + f'\\]'
+            rleft = _seqsym(base,nrr.regex)
+            rright = _seqsym(base,r)
+            info += f'\n\\[{rleft}_n=' \
+                + (f'{n}\\cdot' if n != 1 else '') \
+                + f'{rright}_{f'{{n{s:+}}}' if s != 0 else 'n'}' \
+                + (f'/{d}' if d != 1 else '') + f'\\]'
             for i in range(max(0,-s),max(0,-s)+5):
                 # v1 = evaluate other sequence
                 v1 = nrr.poly(i) * nrr.poly_mult.numerator
@@ -266,21 +272,22 @@ def makePageInfo(nrrset:NrrSetInfo,/) -> NrrPageInfo:
 
 def _nrrfactorstr(nrrset:NrrSetInfo) -> str:
     assert len(nrrset.poly_factors) > 1
-    ret = f'\\[[{nrrset.regex.replace('*','^*')}]_n={nrrset.latex}='
+    base = nrrset.base
+    ret = f'\\[{_seqsym(base,nrrset.regex)}_n={nrrset.latex}='
     if nrrset.poly_mult.numerator != 1:
-        ret += f'{nrrset.poly_mult.numerator}\\times'
+        ret += f'{nrrset.poly_mult.numerator}\\cdot'
     for factor in nrrset.poly_factors:
         ret += f'\\left({factor.latex()}\\right)'
     if nrrset.poly_mult.denominator != 1:
         ret += f'/{nrrset.poly_mult.denominator}'
     ret += f'='
     if nrrset.poly_mult.numerator != 1:
-        ret += f'{nrrset.poly_mult.numerator}\\times'
+        ret += f'{nrrset.poly_mult.numerator}\\cdot'
     for k,s,r in nrrset.poly_factors_equiv:
         ret += f'\\left('
         if k.numerator != 1:
-            ret += f'{k.numerator}\\times'
-        ret += f'[{r.replace('*','^*')}]_{f'{{n{s:+}}}' if s != 0 else 'n'}'
+            ret += f'{k.numerator}\\cdot'
+        ret += f'{_seqsym(base,r)}_{f'{{n{s:+}}}' if s != 0 else 'n'}'
         if k.denominator != 1:
             ret += f'/{k.denominator}'
         ret += f'\\right)'
@@ -297,10 +304,8 @@ if __name__ == '__main__':
                         required=True)
     parser.add_argument('--pattern-list',action='store_true',
                         help='list patterns in stdkmd 5 digit format')
-    parser.add_argument('--python-lambdas',action='store_true',
-                        help='python dict with lambda expressions')
-    parser.add_argument('--python-lambdas-and-latex',action='store_true',
-                        help='python dict with lambdas and latex string')
+    parser.add_argument('--python-prefactor-dict',action='store_true',
+                        help='python dict with lambdas, latex strings, etc')
     parser.add_argument('--python-make-categories',action='store_true',
                         help='output code to make the database categories')
     parser.add_argument('--python-set-descriptions',action='store_true',
@@ -311,9 +316,7 @@ if __name__ == '__main__':
     flag_count = 0
     if args.pattern_list:
         flag_count += 1
-    if args.python_lambdas:
-        flag_count += 1
-    if args.python_lambdas_and_latex:
+    if args.python_prefactor_dict:
         flag_count += 1
     if args.python_make_categories:
         flag_count += 1
@@ -323,8 +326,7 @@ if __name__ == '__main__':
     if flag_count != 1:
         sys.stderr.write('expected exactly one of:\n'
                          '  --pattern-list\n'
-                         '  --python-lambdas\n'
-                         '  --python-lambdas-and-latex\n'
+                         '  --python-prefactor-dict\n'
                          '  --python-make-categories\n'
                          '  --python-set-descriptions\n')
         exit(1)
@@ -336,64 +338,79 @@ if __name__ == '__main__':
             if len(nrrset.poly_factors) == 1:
                 print(nrrset.stdkmd)
 
-    if args.python_lambdas:
-        print('nrr_lambdas = {')
+    if args.python_prefactor_dict:
+        print(f'nrr_data_base_{args.base} = {{')
         for nrrset in nrrdata:
-            if len(nrrset.poly_factors) == 1:
-                print(f'    {repr(nrrset.stdkmd)}: lambda n: {nrrset.expr},')
-            else:
-                pass
-                #print(f'  # {repr(nrrset.stdkmd)}: lambda n: {nrrset.expr},')
-                #factors_str = ' * '.join(f'({f})' for f in nrrset.poly_factors)
-                #print(f'  # factors to: {nrrset.poly_mult} * {factors_str}')
-        print('}')
-
-    if args.python_lambdas_and_latex:
-        print('nrr_data = {')
-        for nrrset in nrrdata:
+            s = nrrset.stdkmd
+            path = f'nrr/{args.base}/{s[0]}/{s}' if args.base > 6 else f'nrr/{args.base}/{s}'
             latex = nrrset.latex
             latex = latex.replace('{','{{')
             latex = latex.replace('}','}}')
             latex = latex.replace('^{{n}}','^{{{n}}}')
             latex = latex.replace('^{{2n}}','^{{{2*n}}}')
             if len(nrrset.poly_factors) == 1:
-                print(f'    {repr(nrrset.stdkmd)}: ((lambda n: {nrrset.expr}), '
-                    f"(lambda n: f{repr(latex)})),")
+                print(f'    {repr(nrrset.stdkmd)}: ({repr(path)}, '
+                      f'(lambda n: {nrrset.expr}), '
+                      f'(lambda n: f{repr(latex)})),')
             else:
                 pass
-                #print(f'  # {repr(nrrset.stdkmd)}: ((lambda n: {nrrset.expr}), '
-                #    f"(lambda n: f{repr(latex)})),")
-                #factors_str = ' * '.join(f'({f})' for f in nrrset.poly_factors)
-                #print(f'  # factors to: {nrrset.poly_mult} * {factors_str}')
-        print('}')
+                print(f'  # {repr(nrrset.stdkmd)}: ({repr(path)}, '
+                      f'(lambda n: {nrrset.expr}), '
+                      f'(lambda n: f{repr(latex)})),')
+                factors_str = ' * '.join(f'({f})' for f in nrrset.poly_factors)
+                print(f'  # factors to: {nrrset.poly_mult} * {factors_str}')
+        print(f'}}')
+
+    # for bases 2-6, put all patterns in the same list
+    # for bases 7-36, split up by starting digit
 
     if args.python_make_categories:
         print('import app.database.categories as dbcat')
         print()
+        digit_paths: dict[str,str] = dict()
         for i in range(1,args.base):
             d = DIGITS[i]
-            print(f"dbcat.createCategory('stdkmd-nrr/{args.base}/{d}', is_table = False, "
-                  f"title = 'First Digit {d}', info = '')")
+            digit_paths[d] = f'nrr/{args.base}' \
+                + (f'/{d}' if args.base > 6 else '')
+            if args.base > 6:
+                print(f"dbcat.createCategory('{digit_paths[d]}', is_table = False, "
+                      f"title = 'First Digit {d}', info = '')")
         prev_digit = '0'
+        all_patterns: set[str] = set()
         for nrrset in nrrdata:
             if len(nrrset.poly_factors) > 1:
                 continue
             s = nrrset.stdkmd
+            all_patterns.add(s)
             if s[0] != prev_digit:
                 print()
                 prev_digit = s[0]
             t = nrrset.regex
             for d in DIGITS[:nrrset.base]:
                 t = t.replace(f'{d}*',_rep(d))
-            print(f"dbcat.createCategory('stdkmd-nrr/{args.base}/{s[0]}/{s}', "
+            print(f"dbcat.createCategory('{digit_paths[s[0]]}/{s}', "
                   f"is_table = True, title = 'Pattern {s}: {t}', info = '')")
             #for nrr in nrrset.equiv_nrrs:
             #    s = nrr.stdkmd
-            #    print(f"dbcat.createCategory('stdkmd-nrr/{s[0]}/{s}', "
+            #    print(f"dbcat.createCategory('nrr/{s[0]}/{s}', "
             #          f"is_table = True, title = 'duplicate', info = '')")
+        print()
+        if args.base > 6:
+            for i in range(1,args.base):
+                d = DIGITS[i]
+                print(f"dbcat.reorderSubcategories({repr(digit_paths[d])},\n"
+                      f"    {repr(list(sorted(s for s in all_patterns if s[0] == d)))})")
+        else:
+            print(f"dbcat.reorderSubcategories('nrr/{args.base}',\n"
+                  f"    {repr(list(sorted(all_patterns)))})")
 
     if args.python_set_descriptions:
         print('import app.database.categories as dbcat')
+        digit_paths: dict[str,str] = dict()
+        for i in range(1,args.base):
+            d = DIGITS[i]
+            digit_paths[d] = f'nrr/{args.base}' \
+                + (f'/{d}' if args.base > 6 else '')
         prev_digit = '0'
         per_digit_factors = {DIGITS[i]:[] for i in range(1,args.base)}
         for nrrset in nrrdata:
@@ -407,25 +424,36 @@ if __name__ == '__main__':
                     per_digit_factors[s[0]].append(_nrrfactorstr(nrr))
             else: # not factorable
                 detail = makePageInfo(nrrset)
-                print(f"dbcat.setCategoryTitle('stdkmd-nrr/{args.base}/{s[0]}/{s}',"
+                print(f"dbcat.setCategoryTitle('{digit_paths[s[0]]}/{s}',"
                     f"{repr(detail.title)})")
                 lines = detail.info.splitlines()
-                print(f"dbcat.setCategoryInfo('stdkmd-nrr/{args.base}/{s[0]}/{s}',")
+                print(f"dbcat.setCategoryInfo('{digit_paths[s[0]]}/{s}',")
                 for i in range(len(lines)):
                     print(f"    {repr(lines[i]+'\n')}"
                         + (')' if i == len(lines)-1 else ''))
         print()
-        for i in range(1,args.base):
-            d = DIGITS[i]
-            print(f"dbcat.setCategoryTitle('stdkmd-nrr/{args.base}/{d}',"
-                  f"'Patterns Starting With {d}')")
-            if len(per_digit_factors[d]) == 0:
-                print(f"dbcat.setCategoryInfo('stdkmd-nrr/{args.base}/{d}',\n"
-                      f"    '<p>Patterns that can be factored: (none)</p>\\n')")
-            else:
-                print(f"dbcat.setCategoryInfo('stdkmd-nrr/{args.base}/{d}',\n"
-                      f"    '<p>Patterns that can be factored:\\n'")
-                last_i = len(per_digit_factors[d])-1
-                for i,data in enumerate(per_digit_factors[d]):
-                    print(f"    {repr(data + ('</p>' if i == last_i else '') + '\n')}"
-                          + (')' if i == last_i else ''))
+        if args.base > 6:
+            for i in range(1,args.base):
+                d = DIGITS[i]
+                print(f"dbcat.setCategoryTitle('nrr/{args.base}/{d}',"
+                      f"'Patterns Starting With {d}')")
+                if len(per_digit_factors[d]) == 0:
+                    print(f"dbcat.setCategoryInfo('nrr/{args.base}/{d}',\n"
+                          f"    '<p>Patterns that can be factored: (none)</p>\\n')")
+                else:
+                    print(f"dbcat.setCategoryInfo('nrr/{args.base}/{d}',\n"
+                          f"    '<p>Patterns that can be factored:\\n'")
+                    last_i = len(per_digit_factors[d])-1
+                    for i,data in enumerate(per_digit_factors[d]):
+                        print(f"    {repr(data + ('</p>' if i == last_i else '') + '\n')}"
+                              + (')' if i == last_i else ''))
+        else:
+            print(f"dbcat.setCategoryInfo('nrr/{args.base}',\n"
+                  f"    '<p>Patterns that can be factored:\\n'")
+            per_digit_factors_list = []
+            for d in per_digit_factors:
+                per_digit_factors_list += per_digit_factors[d]
+            last_i = len(per_digit_factors_list)-1
+            for i,data in enumerate(per_digit_factors_list):
+                print(f"    {repr(data + ('</p>' if i == last_i else '') + '\n')}"
+                      + (')' if i == last_i else ''))
